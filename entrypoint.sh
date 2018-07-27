796e5337f52449e35318ce995fd36676cfa4b7ae
axiowse <axiowse@gmail.com>
2018-07-27 15:40:27 +0800

    init

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+/usr/bin/aria2c \
+    --daemon \
+    --continue=true \
+    --dir=/opt/aria2/downloads \
+    --enable-rpc=true \
+    --rpc-allow-origin-all=true \
+    --rpc-listen-all=true \
+    --rpc-secret=$RPC_SECRET \
+    --rpc-listen-port=6800 && \
+    nginx -g 'daemon off;'
